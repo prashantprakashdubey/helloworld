0aa6a17bd27ef2c2b074871f14f09649cfc0bb9b
Prashant Prakash Dubey™ <61917961+prashantprakashdubey@users.noreply.github.com>
2020-09-08 06:03:06 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,28 @@
+b=500
+PIN=1234
+chances = 3
+while chances>=0:
+    x=int(input('Enter PIN Number: '))
+    if x==PIN:
+        print('****Login Sucessful****')
+        x = int(input('Press 1 to continue: '))
+        if x == 1:
+            print('1. Check Balance')
+            print('2. Withdraw Amount')
+            d=int(input('Enter Your Choice: '))
+            if d==1:
+                print('Your Balance is:')
+                print(b)
+            if d==2:
+                w=int(input('Enter amount to withdraw:'))
+                if w<b:
+                    print('withdraw successful')
+                    b=b-w
+                else:
+                    print('insufficient funds')
+    else:
+        print('***********Invalid Password*************')
+        chances=chances-1
+        if chances==0:
+            print('**************************Account Blocked********************************')
+            break
